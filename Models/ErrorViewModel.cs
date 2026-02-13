dccef7d53b0a9c1b06da0dc8dc24ba94877a75f2
adrinetor86 <adrinetor81@gmail.com>
2026-02-13 11:39:11 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MvcNetCoreEFMultiples.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
